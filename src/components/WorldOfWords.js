fc054d2978571705ef0901e8bd117dc881d23f98
Pehuen Assalone <assalone.pehuen@gmail.com>
2022-06-24 14:30:05 -0300

    otrocommit

--- src/components/WorldOfWords.js
+++ src/components/WorldOfWords.js
@@ -1,4 +1,5 @@
 import './worldOfWords.css'
+import Sass from './img/sass.svg';
 
 export const WorldOfWords = () => {
   return (
@@ -9,11 +10,16 @@ export const WorldOfWords = () => {
       <div className='worldAnimation'>JavaScript</div>
       <div className='worldAnimation'>HTML</div>
       <div className='worldAnimation'>C#</div>
-      <div className='worldAnimation'>SASS</div>
+      <div className='worldAnimation'>Less</div>
       <div className='worldAnimation'>Mongo DB</div>
       <div className='worldAnimation'>TypeScript</div>
-      <div className='worldAnimation'>C</div>
+      <div className='worldAnimation'>Node.Js</div>
       <div className='worldAnimation'>SQL</div>
+      <div className='worldAnimation'>Python</div>
+      <div className='worldAnimation'>Sass</div>
+      <div className='worldAnimation'>Bootstrap</div>
+      <div className='worldAnimation'>Redux</div>
+      <div className='worldAnimation'>C</div>
     </>
   )
 }
