bc145c2def446b352cf3aecfb739a5cf117f4106
Pehuen Assalone <assalone.pehuen@gmail.com>
2022-07-14 12:22:52 -0300

    1407b

--- src/components/MyWork.jsx
+++ src/components/MyWork.jsx
@@ -8,7 +8,6 @@ export const MyWork = () => {
   (document.getElementById("side-menu") || {}).checked = false;
   return (
     <div className="fullPage disableScrollbar noOverflow">
-      <span className="title">My Work</span>
       <Gallery />
 
     </div>
